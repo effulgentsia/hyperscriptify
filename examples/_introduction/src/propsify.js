b27d32dbcc71f5e5353df227d0b66599b0f6689d
effulgentsia <alex.bronstein@acquia.com>
2023-09-13 12:19:38 -0700

    Cleaning up examples

--- examples/_introduction/src/propsify.js
+++
@@ -1,9 +0,0 @@
-import {camelCase, mapKeys, mapValues, set} from "lodash-es";
-import htmlElementAttributeToPropMap from '../../../reactHtmlAttributeToPropertyMap';
-import { basicPropsifyFactory } from "../../..";
-
-const propsify = basicPropsifyFactory({
-  mapKeys, mapValues, set, camelCase, htmlElementAttributeToPropMap
-});
-
-export default propsify;
