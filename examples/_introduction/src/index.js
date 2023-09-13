10d9980fdd716b5da8979d5ebbb505ecd1bf3ead
effulgentsia <alex.bronstein@acquia.com>
2023-09-13 12:21:00 -0700

    Cleaning up examples

--- examples/_introduction/src/index.js
+++ examples/_introduction/src/index.js
@@ -1,8 +1,8 @@
-import hyperscriptify from '../../..';
+import hyperscriptify from 'hyperscriptify';
+import propsify from "hyperscriptify-propsify-standard";
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import components from './components';
-import propsify from "./propsify";
 
 const App = hyperscriptify(document.getElementById('example').content, React.createElement, React.Fragment, components, { propsify });
 
