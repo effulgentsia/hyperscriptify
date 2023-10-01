beb95b8f111b3226f32f7b592d2d541023d996e5
effulgentsia <alex.bronstein@acquia.com>
2023-10-01 14:24:50 -0700

    Added handling for input.value and style conversion

--- propsify/standard/index.js
+++ propsify/standard/index.js
@@ -2,5 +2,29 @@ import factory from '../factory';
 import { camelCase, mapKeys, mapValues, set } from 'lodash-es';
 import htmlElementAttributeToPropMap from './reactHtmlAttributeToPropertyMap';
 
-const propsify = factory({ camelCase, mapKeys, mapValues, set, htmlElementAttributeToPropMap });
-export default propsify;
+const basePropsify = factory({ camelCase, mapKeys, mapValues, set, htmlElementAttributeToPropMap });
+
+export default function propsify( attributes, slots, context ) {
+  const props = basePropsify(attributes, slots, context);
+
+  // In React, setting the value prop of an input element makes the element not
+  // editable, and the defaultValue prop more closely matches the semantics of
+  // the value attribute.
+  if (context.tagName === 'input' && props.defaultValue === undefined && props.value !== undefined ) {
+    props.defaultValue = props.value;
+    delete props.value;
+  }
+
+  // React requires the style prop to be an object rather than a style
+  // attribute string. Instead of parsing the CSS of the style attribute
+  // ourselves, grab it from the DOM element's style property.
+  if (context.element && typeof props.style === 'string') {
+    props.style = Object.entries(context.element.style)
+      // Filter out empty values and numeric keys.
+      .filter( ([ key, value ]) => (value !== '' && isNaN(key)) )
+      // Convert from an array of key/value pairs to a key-indexed object.
+      .reduce( (accumulator, [ key, value ]) => ({ ...accumulator, [key]: value }), {} );
+  }
+
+  return props;
+}
